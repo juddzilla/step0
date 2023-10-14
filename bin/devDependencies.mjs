97600fc445daa871aa185cce80e10117ace6a300
Judd Hendrix <juddhendrix@gmail.com>
2023-10-14 13:38:20 -0700

    yargs test

--- bin/devDependencies.mjs
+++ bin/devDependencies.mjs
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
-import yargs from 'yargs';
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+const argv = yargs(hideBin(process.argv)).argv
 import Install from './install.mjs';
 import Deps from '../src/devDependencies/index.js';
 
-const argv = yargs.argv;
+// const argv = yargs.argv;
 console.log('argv', argv);
 
 // Install(['add', '-WD', ...Deps]);
