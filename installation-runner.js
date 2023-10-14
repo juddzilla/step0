0858de13415a4a8215c79ef680c169d38f592930
Judd Hendrix <juddhendrix@gmail.com>
2023-10-14 11:39:50 -0700

    cleanup

--- installation-runner.js
+++ installation-runner.js
@@ -15,7 +15,7 @@ function execute(command) {
   const exec = spawn('yarn', command);
 
   exec.stdout.on('data', function (data) {
-    console.log('stdout: ' + data.toString());
+    console.log(data.toString());
   });
 
   exec.stderr.on('data', function (data) {
@@ -30,30 +30,13 @@ function execute(command) {
 (async function() {
   const devDeps = path.join(process.cwd(), 'devDependencies', '*.js');
   const files = await find(devDeps);
-  console.log('files', files);
+
   let packages = [];
   await Promise.all(files.map(async (file) => {
     const dep = await import(file);
 
     packages = [...packages, ...dep.default];
   }));
-  console.log('packages', packages);
+
   execute(['add', '-WD', ...packages]);
 })()
-// const packages = [
-//   'eslint',
-//   'eslint-config-airbnb',
-//   'eslint-plugin-import',
-//   'eslint-plugin-jest',
-//   'eslint-plugin-jsx-a11y',
-//   'eslint-plugin-react',
-//   'eslint-plugin-react-hooks',
-// ];
-//
-// const manager = process.argv[2];
-//
-
-// if (manager === 'yarn') {
-//   // -W === ignore-workspace-root-check
-//   execute(['yarn add -WD', ...packages].join(' '));
-// }
